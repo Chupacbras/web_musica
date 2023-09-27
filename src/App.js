b38d3a77c7f6ee4fe508ab4cc718cf63c773ddcd
Chupacbras <albert_furma@hotmail.com>
2023-09-27 15:30:30 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './Estilos/App.css';
+import './Estilos/Toolbar.css';
+import Toolbar from './Toolbar';
+
+function App() {
+  return (
+    <div className='Fondo'>
+      <Toolbar />
+    </div>
+
+  );
+}
+
+export default App;
